609e6379e422d2897f5c16fe0ae73208f76d24ac
Alejandro Narvaez Fajardo <alejonarvaez3489@gmail.com>
2022-12-13 21:06:30 -0500

    primer commit

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,10 +1,15 @@
 const Users = require('./users.models')
 const RecoveryPasswords = require('./recoveryPasswords.models')
+const Conversations = require('./conversations.models')
+const Messages = require('./messages.models')
+const Participants = require('./participants.models')
 
 const initModels = () => {
     //? FK = RecoveryPasswords
     Users.hasMany(RecoveryPasswords)
     RecoveryPasswords.belongsTo(Users)
+
+    
 }
 
 module.exports = initModels
